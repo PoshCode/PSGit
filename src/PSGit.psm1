003abd9e411c44433c2b5f597e31555f5ec3fd34
Joel Bennett <Jaykul@HuddledMasses.org>
2015-05-06 23:34:43 -0400

    Make tests run (and fail, but run)

---
+++ src/PSGit.psm1
@@ -0,0 +1,2 @@
+# TODO: This is just a stub to make the first test pass
+function Get-Status { @{} }
--- test/Status.Steps.ps1
+++ test/Status.Steps.ps1
@@ -1,2 +1,14 @@
 Import-Module PSGit -Force
 
+Given "a new repository" {
+    throw "not implemented"
+}
+
+When "Get-Status is called" {
+    throw "not implemented"
+}
+
+Then "the returned object should show" {
+    param($Table)
+    throw "not implemented"
+}
