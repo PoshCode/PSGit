a74a715c79b418b7538f782a854047d72576b653
Joel Bennett <Jaykul@HuddledMasses.org>
2015-05-08 16:55:20 -0400

    Beautify Test.ps1 failures

--- Test.ps1
+++ Test.ps1
@@ -1,5 +1,5 @@
 [CmdletBinding()]
-param([Switch]$Quiet)
+param([Switch]$Quiet, $FailLimit=0)
 
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
@@ -16,6 +16,9 @@ Write-Verbose "Import-Module $Release\PSGit.psd1" -Verbose:(!$Quiet)
 Import-Module $Release\PSGit.psd1
 
 $Results = Invoke-Gherkin $Pwd\test -ExcludeTag wip -CodeCoverage "$Release\*.ps[m1]*" -PassThru -Quiet:$Quiet
-if($Results.FailedCount -gt 0) {
-    throw "Failed: '$($Results.FailedScenarios.Name -join "', '")'"
+
+if($Results.FailedCount -gt $FailLimit) {
+    $exception = New-Object AggregateException "Failed Scenarios:`n`t`t'$($Results.FailedScenarios.Name -join "'`n`t`t'")'"
+    $errorRecord = New-Object System.Management.Automation.ErrorRecord $exception, "FailedScenarios", "LimitsExceeded", $Results
+    $PSCmdlet.ThrowTerminatingError($errorRecord)
 }
