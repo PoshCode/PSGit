3c54c97012b5cc132b1a7603e3e52dcb20f4753c
Joel Bennett <Jaykul@HuddledMasses.org>
2015-05-09 02:40:54 -0400

    Enhance test launcher

--- Test.ps1
+++ Test.ps1
@@ -1,5 +1,5 @@
 [CmdletBinding()]
-param([Switch]$Quiet, $FailLimit=0)
+param([Switch]$Quiet, [Switch]$ShowWip, $FailLimit=0)
 
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
@@ -15,8 +15,11 @@ $Release = Join-Path $PSScriptRoot $PSGit.ModuleVersion
 Write-Verbose "Import-Module $Release\PSGit.psd1" -Verbose:(!$Quiet)
 Import-Module $Release\PSGit.psd1
 
-$Results = Invoke-Gherkin $Pwd\test -ExcludeTag wip -CodeCoverage "$Release\*.ps[m1]*" -PassThru -Quiet:$Quiet
+$Options = @{}
+if($Quiet) { $Options.Quiet = $Quiet }
+if(!$ShowWip){ $Options.ExcludeTag = @("wip") }
 
+$Results = Invoke-Gherkin $Pwd\test -CodeCoverage "$Release\*.ps[m1]*" -PassThru @Options
 if($Results.FailedCount -gt $FailLimit) {
     $exception = New-Object AggregateException "Failed Scenarios:`n`t`t'$($Results.FailedScenarios.Name -join "'`n`t`t'")'"
     $errorRecord = New-Object System.Management.Automation.ErrorRecord $exception, "FailedScenarios", "LimitsExceeded", $Results
