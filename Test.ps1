7a1a79caad861734698fdbf956678cfc827f8fd5
Joel Bennett <Jaykul@HuddledMasses.org>
2015-05-11 02:26:29 -0400

    Update Test function with a bit more functionality around reporting test results & code coverage

--- Test.ps1
+++ Test.ps1
@@ -1,5 +1,12 @@
 [CmdletBinding()]
-param([Switch]$Quiet, [Switch]$ShowWip, $FailLimit=0)
+param(
+    [Switch]$Quiet,
+    [Switch]$ShowWip,
+    $FailLimit=0
+)
+$TestPath = Join-Path $PSScriptRoot Test
+$OutputPath = Join-Path $PSScriptRoot output
+$SourcePath = Join-Path $PSScriptRoot src
 
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
@@ -12,16 +19,52 @@ Write-Verbose "TESTING $($PSGit.ModuleVersion) build $ENV:APPVEYOR_BUILD_VERSION
 
 $Release = Join-Path $PSScriptRoot $PSGit.ModuleVersion
 
+if(!(Test-Path $OutputPath)) {
+    $null = mkdir $OutputPath -Force
+}
+
 Write-Verbose "Import-Module $Release\PSGit.psd1" -Verbose:(!$Quiet)
 Import-Module $Release\PSGit.psd1
 
-$Options = @{}
+$Options = @{
+    OutputFormat = "NUnitXml"
+    OutputFile = (Join-Path $OutputPath TestResults.xml)
+}
 if($Quiet) { $Options.Quiet = $Quiet }
 if(!$ShowWip){ $Options.ExcludeTag = @("wip") }
 
-$Results = Invoke-Gherkin $Pwd\test -CodeCoverage "$Release\*.ps[m1]*" -PassThru @Options
-if($Results.FailedCount -gt $FailLimit) {
-    $exception = New-Object AggregateException "Failed Scenarios:`n`t`t'$($Results.FailedScenarios.Name -join "'`n`t`t'")'"
+$TestResults = Invoke-Gherkin -Path $TestPath -CodeCoverage "$Release\*.ps[m1]*" -PassThru @Options
+
+$script:failedTestsCount = 0
+$script:passedTestsCount = 0
+foreach($result in $TestResults)
+{
+    if($result)
+    {
+        $script:failedTestsCount += $result.FailedCount 
+        $script:passedTestsCount += $result.PassedCount 
+        $CodeCoverageTitle = 'Code Coverage {0:F1}%'  -f (100 * ($result.CodeCoverage.NumberOfCommandsExecuted / $result.CodeCoverage.NumberOfCommandsAnalyzed))
+
+        if($result.CodeCoverage.MissedCommands.Count -gt 0) {
+            $result.CodeCoverage.MissedCommands |
+                ConvertTo-FormattedHtml -title $CodeCoverageTitle | 
+                Out-File (Join-Path $OutputPath "CodeCoverage-${ENV:APPVEYOR_BUILD_VERSION}.html")
+        }
+        if(${ENV:CodeCovIoToken})
+        {
+            &"$TestPath\Send-CodeCov" -CodeCoverage $result.CodeCoverage -RepositoryRoot $PSScriptRoot -OutputPath $OutputPath -token ${ENV:CodeCovIoToken}
+        }
+    }
+}
+
+if((Test-Path $Options.OutputFile)) {
+    $wc = New-Object 'System.Net.WebClient'
+    $wc.UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", $Options.OutputFile)
+}
+
+if($FailedTestsCount -gt $FailLimit) {
+    $exception = New-Object AggregateException "Failed Scenarios:`n`t`t'$($TestResults.FailedScenarios.Name -join "'`n`t`t'")'"
     $errorRecord = New-Object System.Management.Automation.ErrorRecord $exception, "FailedScenarios", "LimitsExceeded", $Results
     $PSCmdlet.ThrowTerminatingError($errorRecord)
 }
+
