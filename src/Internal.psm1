0443dbaf61c103fb8e2799480f97ac661766eaa9
Justin <jrich523@gmail.com>
2015-05-22 12:31:59 -0700

    adding a way to write tips/information out as an internal function

---
+++ src/Internal.psm1
@@ -0,0 +1,46 @@
+﻿function WriteMessage
+{
+    [CmdletBinding()]
+    Param
+    (
+        # The Type of message you'd like, its just a prefix to the message.
+        [Parameter(Mandatory=$true,Position=0)]
+        [string]
+        $Type,
+
+        # The message to display
+        [Parameter(Mandatory=$true,Position=1)]
+        [string]
+        $Message,
+        
+        # Color of the message, it will default to the hosts' verbose color.
+        [Parameter(Position=2)]
+        
+        $ForegroundColor = $host.PrivateData.VerboseForegroundColor,
+        # Background color of the message, it will default to the hosts' verbose color.
+        [Parameter(Position=3)] 
+        $BackgroundColor = $host.PrivateData.VerboseBackgroundColor
+    )
+
+    if($ForegroundColor -is [System.Windows.Media.Color]){ $ForegroundColor = ConvertColor $ForegroundColor }
+    if($BackgroundColor -is [System.Windows.Media.Color])
+    {
+        # if its a transparent color
+        if($BackgroundColor -eq "#00FFFFFF") {$BackgroundColor = $host.PrivateData.ConsolePaneBackgroundColor}
+        $BackgroundColor = ConvertColor $BackgroundColor
+    }
+
+    #todo check to see if the preference is on or off
+    
+    Write-Host "$($Type.ToUpper()): $Message" -ForegroundColor $ForegroundColor -BackgroundColor $BackgroundColor 
+}
+
+
+function ConvertColor($color)
+{
+    [int]$bright = if($color.R -gt 128 -bor $color.G -gt 128 -bor $color.B -gt 128){8}else{0}
+    [int]$r = if($color.R -gt 64){4}else{0}
+    [int]$g = if($color.G -gt 64){2}else{0}
+    [int]$b = if($color.B -gt 64){1}else{0}
+    [consolecolor]($bright -bor $r -bor $g -bor $b)
+}
