66cc9e4f51544eb79abe23b8aa06f8bcc279df1e
Justin <jrich523@gmail.com>
2015-05-27 10:23:58 -0700

    adding tests for internal functions

---
+++ test/Internal.Steps.ps1
@@ -0,0 +1,12 @@
+﻿When "WriteMessage ((?<type>\S+)\s+(?<message>\S+))? ?is called" {
+    param($type,$message)
+    if($type) {
+        $script:result = &(gmo psgit){WriteMessage -type test -message $message -InformationVariable information -InformationAction SilentlyContinue;$information }
+    } else {
+        $script:result = &(gmo psgit){WriteMessage -type Tip -message test  -InformationVariable information -InformationAction SilentlyContinue; $information }
+    }
+}
+
+When 'ConvertColor is called' {
+        $script:result = &(gmo psgit){ConvertColor -color '#00ff0000' }
+}
