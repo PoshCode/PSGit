ee6428bf94e601f62376855b24b53b3cc3882aa5
Justin <jrich523@gmail.com>
2015-05-28 11:20:43 -0700

    created a basic test with mocking write-host, needs to be more detailed

--- test/Internal.Steps.ps1
+++ test/Internal.Steps.ps1
@@ -1,12 +1,22 @@
-﻿When "WriteMessage ((?<type>\S+)\s+(?<message>\S+))? ?is called" {
+﻿<#
+When "WriteMessage ((?<type>\S+)\s+(?<message>\S+))? ?is called" {
     param($type,$message)
+    Mock Write-Host -ParameterFilter {$object -eq "TIP: test"} -Verifiable 
+    
     if($type) {
         $script:result = &(gmo psgit){WriteMessage -type test -message $message -InformationVariable information -InformationAction SilentlyContinue;$information }
     } else {
-        $script:result = &(gmo psgit){WriteMessage -type Tip -message test  -InformationVariable information -InformationAction SilentlyContinue; $information }
+        $script:result = &(gmo psgit){WriteMessage -type Tip -message test  SilentlyContinue; $information }
     }
+}
+#>
+
+When "WriteMessage is called" {
+    Mock Write-Host {return "TIP: test"} -ParameterFilter {$object -eq "TIP: test"} -Verifiable -ModuleName psgit
+    $script:result = &(gmo psgit){WriteMessage -type Tip -message test  }
 }
 
 When 'ConvertColor is called' {
         $script:result = &(gmo psgit){ConvertColor -color red }
 }
+
