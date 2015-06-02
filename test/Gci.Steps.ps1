3ea3414e839a6b4aaf5a7f7aa3ec29b48635f904
Justin <jrich523@gmail.com>
2015-06-02 11:50:35 -0700

    adding depth to the tests

--- test/Gci.Steps.ps1
+++ test/Gci.Steps.ps1
@@ -1,1 +1,24 @@
-## steps
+When 'Get-ChildItem is called' {
+    $script:repo = Convert-Path TestDrive:\
+    Push-Location TestDrive:\
+    [Environment]::CurrentDirectory = $repo
+    $script:result = Get-ChildItem
+}
+
+
+Then 'the resulting object should not have a Changed property' {
+
+    if($script:result | get-member -membertype noteproperty | ? name -eq Changed) {
+        throw "Found Property, showing Git status when we are not in a repo"
+    }
+}
+
+Then 'the resulting object should have a Changed property' {
+
+    if($script:result | get-member -membertype noteproperty | ? name -eq Changed) {
+        
+    }
+    else {
+        throw "Property not found!"
+    }
+}
